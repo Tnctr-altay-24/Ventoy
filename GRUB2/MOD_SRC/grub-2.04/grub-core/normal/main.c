1f1be72a4e593a85f1ec028bd1d78c431e2f277d
Tnctr-altay-24 <Tnctr-altay-24@yandex.com>
2026-09-07 15:04:19 +0300

    Update main.c

--- GRUB2/MOD_SRC/grub-2.04/grub-core/normal/main.c
+++ GRUB2/MOD_SRC/grub-2.04/grub-core/normal/main.c
@@ -39,6 +39,17 @@ GRUB_MOD_LICENSE ("GPLv3+");
 
 #define GRUB_DEFAULT_HISTORY_SIZE	50
 
+#define VENTOY_WELCOME1 "        _     _          __   __ "
+#define VENTOY_WELCOME2 "       | |   | |         \\ \\ / /"
+#define VENTOY_WELCOME3 "  __ _ | |   | |_    __ _ \\_v_/"
+#define VENTOY_WELCOME4 " / _' || |   | __|  / _' | | |"
+#define VENTOY_WELCOME5 "| (_| || |__ | |__ | (_| | | |"
+#define VENTOY_WELCOME6 " \\__,_| \\___| \\___| \\__,_| |_|"
+
+#define VENTOY_WELCOME7  "  Copyright © 2026-2027 aLTay  "
+
+static int nested_level = 0;
+
 static int nested_level = 0;
 int grub_normal_exit_level = 0;
 
@@ -276,6 +287,19 @@ grub_normal_execute (const char *config, int nested, int batch)
     }
 
   grub_boot_time ("Executing config file");
+
+  grub_setcolorstate (GRUB_TERM_COLOR_HIGHLIGHT);
+  
+  grub_printf ("%s\n", VENTOY_WELCOME1);
+  grub_printf ("%s\n", VENTOY_WELCOME2);
+  grub_printf ("%s\n", VENTOY_WELCOME3);
+  grub_printf ("%s\n", VENTOY_WELCOME4);
+  grub_printf ("%s\n", VENTOY_WELCOME5);
+  grub_printf ("%s\n", VENTOY_WELCOME6);
+
+  grub_printf ("%s\n", VENTOY_WELCOME7);
+
+  grub_setcolorstate (GRUB_TERM_COLOR_STANDARD);
 
   if (config)
     {
