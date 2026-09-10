cf4914cac969421cd5a02121d65fe481a8b2438a
Tnctr-altay-24 <Tnctr-altay-24@yandex.com>
2026-09-10 09:32:13 +0300

    Update Ventoy version and language display formatting

--- GRUB2/MOD_SRC/grub-2.04/grub-core/normal/main.c
+++ GRUB2/MOD_SRC/grub-2.04/grub-core/normal/main.c
@@ -531,11 +531,12 @@ grub_cmd_ventoy_about (struct grub_command *cmd __attribute__ ((unused)),
   grub_printf ("\n");
   grub_printf ("%s\n", VENTOY_WELCOME7);
   grub_printf ("\n");
-  grub_printf ("Ventoy Versiyon: VTOY_TEXT_MENU_VER");
+  grub_printf("Ventoy Versiyon: %s\n",
+            grub_env_get("VTOY_TEXT_MENU_VER"));
   grub_printf ("\n");
-  grub_printf ("Dil: vtCurLang");
+  grub_printf ("Dil: %s\n", vtCurLang);
   grub_printf ("\n");
-  /* grub_printf (" VTLANG_RETURN_PREVIOUS...\n"); */
+  grub_printf("%s\n", VTLANG_RETURN_PREVIOUS);
 
   while (grub_getkey () != GRUB_TERM_ESC)
   grub_cls ();
