336b02e42abdcd8c717d9ea37bcfa978fec2b56b
Tnctr-altay-24 <Tnctr-altay-24@yandex.com>
2026-09-10 11:31:45 +0300

    Update main.c

--- GRUB2/MOD_SRC/grub-2.04/grub-core/normal/main.c
+++ GRUB2/MOD_SRC/grub-2.04/grub-core/normal/main.c
@@ -531,9 +531,11 @@ grub_cmd_ventoy_about (struct grub_command *cmd __attribute__ ((unused)),
   grub_printf ("\n");
   grub_printf ("%s\n", VENTOY_WELCOME7);
   grub_printf ("\n");
-  grub_printf("%s\n", grub_env_get("VTOY_TEXT_MENU_VER"));
-  grub_printf ("\n");
   grub_printf("Dil:%s\n", grub_env_get("VTOY_MENU_LANGUAGE"));
+  grub_printf ("\n");
+  grub_printf("Platform:%s\n", grub_env_get("VTOY_TEXT_MENU_VER"));
+  grub_printf ("\n");
+  grub_printf("Mimari versiyon:%s\n", grub_env_get("VTOY_TEXT_MENU_VER"));
   grub_printf ("\n");
   grub_printf("%s\n", (ESC) ile Ana Menu);
 
